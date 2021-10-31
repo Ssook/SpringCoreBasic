53c9d7d62a211a4932e77c608e94dc9094d99e29
Ssook <cesces333@naver.com>
2021-11-01 01:44:03 +0900

    [test]@Qualifier, @Primary 사용해서 중복된 빈 처리

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,13 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Primary // 같은 빈이 조회 될 경우 우선 순위 결정
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10;
 
