c67033992280e840ce12db299382a98f12bfa64f
Ssook <cesces333@naver.com>
2021-11-02 23:30:14 +0900

    [test]싱글톤빈과 프로토타입 빈 차이 공부

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,34 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+        Assertions.assertThat(bean1).isNotSameAs(bean2);
+        ac.close();
+    }
+
+    // 프로토타입 스코프는 필요할 때 생성되고, 그 후로는 스프링 컨테이너가 관리를 하지 않기에 종료 콜백 실행이 안된다.
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("prototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("prototypeBean.destroy");
+        }
+    }
+}
